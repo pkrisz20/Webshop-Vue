47dd48b8076159485f869b8acb26297251e99171
pkrisz20 <krisztianpasztor11@gmail.com>
2022-10-04 10:54:35 +0200

    Init commit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
