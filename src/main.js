c65a72bfac17ee9bf8551be6a5c3c9f96bdfadf6
pkrisz20 <krisztianpasztor11@gmail.com>
2022-10-05 16:06:58 +0200

    Added global js functions

--- src/main.js
+++ src/main.js
@@ -5,6 +5,46 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+Vue.mixin({
+  methods: {
+    gSelector (selector, parent = document) {
+      return parent.querySelector(selector);
+    },
+    gSelectorAll (selector, parent = document) {
+      return [...parent.querySelectorAll(selector)];
+    },
+    getFirst (array, unit = 1) {
+      if (unit === 1) return array[0];
+      return array.filter((_, index) => index < unit);
+    },
+    getLast (array, unit = 1) {
+      if (unit === 1) return array[array.length - 1];
+      return array.filter((_, index) => array.length - index <= unit);
+    },
+    getOnly (array, key) {
+      return array.map(element => element[key]);
+    },
+    groupBy (array, key) {
+      return array.reduce((group, element) => {
+        const keyValue = element[key];
+        return { ...group, [keyValue]: [...(group[keyValue] ?? []), element] }
+      }, {});
+    },
+    formatNumber (number) {
+      const numberFormatter = new Intl.NumberFormat(undefined);
+      return numberFormatter.format(number);
+    },
+    formatCurrency (number) {
+      const currencyFormatter = new Intl.NumberFormat(undefined, { currency: "USD", style: "currency" });
+      return currencyFormatter.format(number);
+    },
+    formatCompactNumber (number) {
+      const compactNumberFormatter = new Intl.NumberFormat(undefined, { notation: "compact" });
+      return compactNumberFormatter.format(number);
+    },
+  }
+});
+
 new Vue({
   router,
   store,
